d90c799a069fd3319615ccdeec1f8396303786d6
Antonincks <Ifro@GJM-LAB7-11.ifro.local>
2024-10-02 10:11:20 -0400

    continuação database

--- App/Libraries/Database.php
+++ App/Libraries/Database.php
@@ -6,6 +6,7 @@ class Database{
     private $banco = "blogaula";
     private $porta = "3306"; //verificar a porta do seu banco
     private $dbh;
+    private $stmt;
 
     public function __construct(){
         //fonte de dados ou DNS que contém as informações para conectar ao banco de dados.
@@ -27,4 +28,57 @@ class Database{
         }//fim do catch
 
     }//fim do metodo construtor
-}
+
+    //prepara o statement com query
+    public function query($sql){
+        //prepara a consulta sql
+        $this->stmt= $this->dbh->prepare($sql);
+    }//fim da função query
+    //vincula um valor a um parametro
+    public function bind($parametro, $valor, $tipo= null){
+        if(is_null($tipo)):
+            switch(true):
+                case is_int($valor):
+                    $tipo = PDO::PARAM_INT;
+                    break;
+                case is_bool($valor):
+                    $tipo = PDO::PARAM_BOOL;
+                    break;
+                case is_null($valor):
+                    $tipo = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $tipo = PDO::PARAM_STR;
+                endswitch;
+        endif;
+        $this->stmt->bindValue($parametro, $valor, $tipo);
+    }//fim da função bind
+    
+    //executa prepared statement
+    public function executa(){
+        return $this->stmt->execute();
+    }//fim da função executa 
+
+    //obtem um unico registro
+    public function resultado(){
+        $this->executa();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }//fim da função resultado
+
+    //ob' tem um conjunto de registros
+    public function resultados(){
+        $this->executa();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ)
+    }//fim da função resultados 
+
+    //retorna o número de linhas afetadas pela ultima instrução SQL
+    public function totalResultados(){
+        return $this->stmt->rowCount();
+    }//fim da função totalResultados
+
+    //retorn o ultimo ID inserido no banco de dados
+    public function ultimoIdInserido(){
+        return $this->dbh->lastInsertId();
+    }//fim da funcao ultimoIdInserido
+
+}//fim da classe database
